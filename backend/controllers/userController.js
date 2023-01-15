a2c5a3f3818ea0ca05b697d8efe2fb7dbb2d94ea
Rami B <ramibasheer9@gmail.com>
2023-01-15 21:32:02 +0100

    register functionalty

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,17 +12,32 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // Check if user already exists
   const userExists = await User.findOne({ email });
-    if (userExists) {
-        res.status(400);
-        throw new Error("User already exists");
-    }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
-  res.json({ message: "User registered" });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
 });
 
 const loginUser = asyncHandler(async (req, res) => {
