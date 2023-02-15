f685eacd53aee20aea45a7f79f2c3422deaa0455
Rami B <ramibasheer9@gmail.com>
2023-02-15 10:59:06 +0100

    apply comment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const {errorHandler} = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-
+// to coonect to the database
 connectDB();
 
 const app = express();
